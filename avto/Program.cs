ef4fcb4447e98a1c3e682b61f53c3929ef22be59
Rolix <130452450+Ro1ix@users.noreply.github.com>
2023-10-02 10:40:54 +0300

    v0.1 beta

---
+++ avto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class avto
+    {
+        private string nom;
+        private int bak;
+        private double ras = new double();
+        void info(string nom, float bak, float ras)
+        {
+            this.nom = Console.ReadLine();
+            this.bak = Convert.ToInt32(Console.ReadLine());
+            this.ras = Convert.ToDouble(Console.ReadLine());
+        }
+        void vivod()
+        {
+            Console.WriteLine(nom);
+            Console.WriteLine(bak);
+            Console.WriteLine(ras);
+        }
+
+    }
+}
